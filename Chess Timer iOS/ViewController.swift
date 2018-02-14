73ea60cbfc8d3de207995df1a0e37cd3be7064ef
Spencer Perkins <hello@spencerperkins.me>
2018-02-14 11:47:27 -0800

    Starting to figure out the timer

--- Chess Timer iOS/ViewController.swift
+++ Chess Timer iOS/ViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    struct Config {
+        static var timerLength: Double = 65
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var timeRemaining = Config.timerLength {
+        didSet {
+            timerLabel.text = formattedTime(timeRemaining)
+        }
     }
 
+    var gameTimer = Timer()
+    var timerIsRunning = false
 
+    @IBOutlet weak var timerLabel: UILabel!
+
+    @IBAction func startButton(_ sender: UIButton) {
+        if timerIsRunning == false {
+            startTimer()
+            timerIsRunning = true
+        }
+    }
+
+    @IBAction func resetButton(_ sender: UIButton) {
+        timeRemaining = Config.timerLength
+    }
+
+    func startTimer() {
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self,   selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateTimer() {
+        if timeRemaining > 0 {
+            timeRemaining -= 0.1
+            if timeRemaining <= 0 {
+                timeRemaining = 0
+                view.backgroundColor = #colorLiteral(red: 1, green: 0.06543179506, blue: 0, alpha: 0.741411601)
+            }
+        }
+    }
+
+    func formattedTime(_ time: Double) -> String {
+        let minutes = Int(time / 60)
+        let seconds = Int(time - Double(minutes) * 60)
+        let milliseconds = Int((time - Double(minutes) * 60 - Double(seconds)) * 10)
+        let formattedString: String!
+
+        if minutes == 0 {
+            formattedString = "\(seconds).\(milliseconds)"
+        } else {
+            if minutes < 10 {
+                formattedString =  "\(minutes):0\(seconds)"
+            } else {
+                formattedString = "\(minutes):\(seconds)"
+            }
+        }
+        return formattedString
+    }
 }
 
