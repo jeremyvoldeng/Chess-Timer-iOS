b15c0e8fae66b4ac9c0e74140a45a38dd813228d
Jeremy Voldeng <jeremyvoldeng@gmail.com>
2018-02-14 10:23:01 -0800

    Initial Commit

---
+++ Chess Timer iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chess Timer iOS
+//
+//  Created by Jeremy Voldeng on 2018-02-14.
+//  Copyright © 2018 Jeremy Voldeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
